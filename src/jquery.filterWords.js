1c07dff368e14a433844136ae45d19553b544931
Ruchira Chamara <bandararuchira3@gmail.com>
2012-10-02 14:50:53 +0530

    draft version 1.1

--- src/jquery.filterWords.js
+++ src/jquery.filterWords.js
@@ -9,16 +9,29 @@
 (function($) {
 
 	var filterWords = {
-		init: function(jsonArray){
-			$.each(jsonArray, function(i){
-				
+		init: function(jsonArray = null, selectorWrapper = null, inputType = null){
+			this.extendedJsonArray = $.extend({}, $.filterWords.defaultJsonArray);
+			var extendedJsonArr = this.extendedJsonArray;			
+			var extendedInputType = ((inputType != null) && (inputType != "")) ? inputType : $.filterWords.defaultInputType;			
+			this.htmlWrapperForAllFliters = ((selectorWrapper != null) && (selectorWrapper != "")) ? selectorWrapper : $.filterWords.defaultWrapperDivClass;
+			var htmlAllFilterInputs = "";
+			$.each(extendedJsonArr, function(i){
+				htmlAllFilterInputs += "<Label>" + extendedJsonArr[i] + "<input type='" + extendedInputType + "' value='" + extendedJsonArr[i] + "' /></Label><br />";
 			});
+			$($("<div class='" + this.htmlWrapperForAllFliters + "'></div>").append(htmlAllFilterInputs)).appendTo('body');
+			this.startFiltering();
+		},
+		startFiltering: function(){
+			
 		}
 	};
 	
-	$.filterWords = function(jsonArray){
+	$.filterWords = function(jsonArray, selectorWrapper, inputType){
 		var obj = Object.create(filterWords);
-		obj.init(jsonArray);
+		obj.init(jsonArray, selectorWrapper, inputType);
 	};
 	
+	$.filterWords.defaultJsonArray = ["nissan","toyota", "hyundai", "maruti", "suzuki", "merzidis-benz", "bmw", "audi", "isuzu"];
+	$.filterWords.defaultInputType = "checkbox";
+	$.filterWords.defaultWrapperDivClass = "wordsFilterWrapper";	
 }(jQuery));
