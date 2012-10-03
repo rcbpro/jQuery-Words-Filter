555e17fb50138b72b1b1e9f959c18b45af24c517
Ruchira Chamara <bandararuchira3@gmail.com>
2012-10-03 11:01:44 +0530

    draft version 1.1

--- src/jquery.filterWords.js
+++ src/jquery.filterWords.js
@@ -9,8 +9,9 @@
 (function($) {
 
 	var filterWords = {
-		init: function(jsonArray = null, selectorWrapper = null, inputType = null){
+		init: function(jsonArray = null, selectorWrapper = null, selectorWrapperCss = null, inputType = null){
 			this.extendedJsonArray = $.extend({}, $.filterWords.defaultJsonArray);
+			this.selectorWrapperCss = $.extend({}, $.filterWords.selectorWrapperCss);			
 			var extendedJsonArr = this.extendedJsonArray;			
 			var extendedInputType = ((inputType != null) && (inputType != "")) ? inputType : $.filterWords.defaultInputType;			
 			this.htmlWrapperForAllFliters = ((selectorWrapper != null) && (selectorWrapper != "")) ? selectorWrapper : $.filterWords.defaultWrapperDivClass;
@@ -26,12 +27,30 @@
 		}
 	};
 	
-	$.filterWords = function(jsonArray, selectorWrapper, inputType){
+	$.filterWords = function(jsonArray, selectorWrapper, selectorWrapperCss, inputType){
 		var obj = Object.create(filterWords);
-		obj.init(jsonArray, selectorWrapper, inputType);
+		obj.init(jsonArray, selectorWrapper, selectorWrapperCss, inputType);
 	};
 	
 	$.filterWords.defaultJsonArray = ["nissan","toyota", "hyundai", "maruti", "suzuki", "merzidis-benz", "bmw", "audi", "isuzu"];
 	$.filterWords.defaultInputType = "checkbox";
-	$.filterWords.defaultWrapperDivClass = "wordsFilterWrapper";	
+	$.filterWords.defaultWrapperDivClass = ["wordsFilterWrapper", "";
+	$.filterWords.defaultCss =  [
+								  {
+									  'width': '300px', 
+									  'height': '300px', 
+									  'background-color':'#CCC', 
+									  'font-size':'12px'
+								  },
+								  {
+									  'width': '200px', 
+									  'height': '300px', 
+									  'background-color':'#CFC'
+								  },
+								  {
+									  'width': '100px', 
+									  'height': '300px', 
+									  'background-color':'#3FF'
+								  }
+							   ];
 }(jQuery));
